53b869ce6186cbec0b560ec86aa26d0b38d67dd7
Tolon05 <tolon0005@gmail.com>
2025-04-17 22:02:31 +0600

    Delete app directory

--- app/scriptss/script.js
+++
@@ -1,9 +0,0 @@
-function togglePassword() {
-    const passwordField = document.getElementById("password");
-    const isHidden = passwordField.type === "password";
-
-    passwordField.type = isHidden ? "text" : "password";
-
-    document.querySelector(".icon-eye").style.display = isHidden ? "none" : "inline";
-    document.querySelector(".icon-eye-off").style.display = isHidden ? "inline" : "none";
-}
