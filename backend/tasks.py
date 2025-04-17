b0b8f3251347201de00a268dc2adf4814d4d6dd4
Tolon05 <tolon0005@gmail.com>
2025-04-17 22:02:43 +0600

    Delete backend directory

--- backend/tasks.py
+++
@@ -1,6 +0,0 @@
-from backend.celery_worker import celery
-from backend.email_utils import send_verification_email
-
-@celery.task
-async def send_verification_email_task(to_email: str, code: str):
-    await send_verification_email(to_email, code)
